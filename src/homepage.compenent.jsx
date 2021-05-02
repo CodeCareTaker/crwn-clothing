0ee28268c828036586ffb137d5e30e5c0c8a44c2
CodeCareTaker <cctprojects@gmail.com>
2021-05-02 04:35:50 -0400

    created project, added pages directory, homepage files

---
+++ src/homepage.compenent.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>HATS</div>
+          <span className='subtitle'>SHOP NOW</div>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>JACKETS</div>
+          <span className='subtitle'>SHOP NOW</div>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>SNEAKERS</div>
+          <span className='subtitle'>SHOP NOW</div>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>WOMENS</div>
+          <span className='subtitle'>SHOP NOW</div>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>MENS</div>
+          <span className='subtitle'>SHOP NOW</div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
