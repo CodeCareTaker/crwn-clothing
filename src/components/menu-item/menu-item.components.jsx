39e03e415e0d9018fa101ce790e6c38aa85b8103
CodeCareTaker <cctprojects@gmail.com>
2021-05-02 06:31:50 -0400

    added menu-item, and directory components

--- src/components/menu-item/menu-item.components.jsx
+++ src/components/menu-item/menu-item.components.jsx
@@ -6,7 +6,7 @@ const MenuItem = ({ title }) => (
   <div style={{
       backgroundImage: `url(${imageUrl})`
   }} 
-  className='menu-item'>
+  className={`${size} menu-item`}>
   <div className='content'>
     <h1 className='title'>{title}</h1>
     <span className='subtitle'>SHOP NOW</span>
