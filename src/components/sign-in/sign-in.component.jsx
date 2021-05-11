efa8bd7ee8e9a7469977bcd12707ca4c408804e3
CodeCareTaker <cctprojects@gmail.com>
2021-05-11 09:35:20 -0400

    added redux, and implemented userReducers and userActions to header and components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import CustomButton from '../custom-button/custom-button.component';
 import FormInput from '../form-input/form-input.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -17,11 +17,18 @@ class SignIn extends React.Component{
     }
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({email: '', password: '' })
-  }
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword( email, password );
+      this.setState({email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   handleChange = event => {
     const { value, name } = event.target;
@@ -54,7 +61,7 @@ class SignIn extends React.Component{
           />
 
           <CustomButton type='submit'> Sign In </CustomButton>
-          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+          <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>
             {' '}
             Sign in with Google{' '}
           </CustomButton>
